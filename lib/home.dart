c4455fbc23e69580883429e92671cac058e6cc49
alrieq <alrieq@live.com>
2023-05-05 18:14:49 +0300

    remove demo app

---
+++ lib/home.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(context) {
+    return const MaterialApp(
+      home: Text('This is the app'),
+    );
+  }
+}
