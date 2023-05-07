4a0c72dc01b46667d8a668751256f3111fd67494
alrieq <alrieq@live.com>
2023-05-07 21:02:07 +0300

    create login page and firebase

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,13 +1,131 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:senior_project/pages/select_class.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  void afterLogin() {
+    Navigator.of(context).pushReplacement(MaterialPageRoute(
+      builder: (context) => const SelectClass(),
+    ));
+  }
+
+  static Future<User?> loginUsingEmailPassword(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No User found for that email');
+      }
+    }
+
+    return user;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("LoginPage"),
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Attendance System',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text(
+            'Login to your account',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 44,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 44,
+          ),
+          TextField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: 'User Email',
+              prefixIcon: Icon(
+                Icons.mail,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 26,
+          ),
+          TextField(
+            controller: _passwordController,
+            obscureText: true,
+            decoration: const InputDecoration(
+              hintText: 'User Password',
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          const Text(
+            "Dont't remember your password?",
+            style: TextStyle(color: Colors.blue),
+          ),
+          const SizedBox(
+            height: 88,
+          ),
+          Container(
+            width: double.infinity,
+            child: RawMaterialButton(
+              fillColor: const Color(0xFF0069FE),
+              elevation: 0,
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              onPressed: () async {
+                User? user = await loginUsingEmailPassword(
+                    email: _emailController.text,
+                    password: _passwordController.text,
+                    context: context);
+                print(user);
+                if (user != null) {
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => const SelectClass(),
+                  ));
+                }
+              },
+              child: const Text(
+                'Login',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,17 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:senior_project/pages/login.dart';
-import 'package:senior_project/pages/select_class.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var x = 1;
-    if (x == 1) {
-      return const SelectClass();
-    } else {
-      return const LoginPage();
+    Future<FirebaseApp> _initializeFirebase() async {
+      FirebaseApp firebaseApp = await Firebase.initializeApp();
+      return firebaseApp;
     }
+
+    return Scaffold(
+      body: FutureBuilder(
+        future: _initializeFirebase(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return const LoginPage();
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
   }
 }
