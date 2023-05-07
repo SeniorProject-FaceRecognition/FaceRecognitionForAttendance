b6b50c567e1f0f9beef859b7bc070fbf4aa1f184
AbdullahAli1999 <iifamxx@gmail.com>
2023-05-07 12:15:46 -0700

    Student page

---
+++ lib/pages/student_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class StudentPage extends StatefulWidget {
+  const StudentPage({super.key});
+
+  @override
+  State<StudentPage> createState() => _StudentPageState();
+}
+
+class _StudentPageState extends State<StudentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+  
